2c41e65bb31b1efe80a2145b95490beba1c59f55
ahes <marcin@rootnode.net>
2012-06-12 10:23:31 +0200

    MySQL container configuration

--- config/web/mysql/install.sh
+++ config/web/mysql/install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+# Installation script 
+# MySQL container configuration
+
+# Exit on error
 set -e
 
+# Debug information
+set -x
+
+# Destination directory
 ROOTFS="/lxc/system/mysql/rootfs"
 
 # MySQL config file
-cp my.cnf $ROOTFS/etc/mysql/my.cnf
+cp etc/mysql/my.cnf  $ROOTFS/etc/mysql/my.cnf
+
+# Daemontools scripts
+cp -r etc/service/agent $ROOTFS/etc/service/
+chmod 700                $ROOTFS/etc/service/agent/run
+
+exit
